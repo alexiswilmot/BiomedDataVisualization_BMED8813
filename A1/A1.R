29c341f172fd0ea5ae8fffdc15094412d13283f2
Wilmot, Alexis Irene <alexiswilmot@gatech.edu>
2025-01-30 12:46:03 -0500

    r update

---
+++ A1/A1.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+library(GGally)
+library(reshape2)
+library(gridExtra)
+
+
+df <- read.csv("medical_costs.csv")
+
+# average medical cost by age
+age <- df$Age
+avg_medical_age <- aggregate(cost ~ Age, data = df, FUN = mean)
+cost <- df$Medical.Cost
+
+ggplot(avg_medical_age, aes(x=Age, y=cost)) +
+  geom_line(color="darkgreen") +
+  geom_point(color = "darkgreen") +
+  labs(title = "Average Medical Cost by Age", x = "Age", y = "Average Medical Cost") + 
+  theme_minimal() + 
+  theme(panel.grid.major = element_line(color = "gray"))
+
+
